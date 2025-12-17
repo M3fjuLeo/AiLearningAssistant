4c8211ed46db06ce2e60184145eb35e5c0c99328
M3fjuLeo <mateuszropek@onet.pl>
2025-12-17 12:53:48 +0100

    adding login controller

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
-import dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
+
+// import dotenv from "dotenv";
+// dotenv.config();
 
 import express from "express";
 import cors from "cors";
